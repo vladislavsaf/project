89ccc06933ce0476f1b601ce90fe86310946a59d
vladislav.safonov <safonix@bk.ru>
2023-01-11 20:40:49 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//alert('Hello');
+
+//const result = confirm('Are your hear?');
+//console.log(result);
+
+//const answer = prompt('Вам есть 18?', '');
+//console.log(typeof answer);
+
+const answers = [];
+
+//answers[0] = prompt('Как ваше имя?');
+//answers[1] = prompt('Как ваша Фамилия?');
+//answers[2] = prompt('Сколько Вам лет?');
+
+//document.write(answers);
+
+console.log(typeof answers);
