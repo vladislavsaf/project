05874ca60c3d173eaf9a2dca4fd6039d35171d64
vladislav.safonov <safonix@bk.ru>
2023-01-11 21:58:05 +0300

    First Web Application

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastWatchedFilmFirst = prompt('Один из просмотренных фильмов?', '');
+const filmRaitingFirst = prompt('Насколько оцените его', '');
+const lastWatchedFilmSecond = prompt('Один из просмотренных фильмов?', '');
+const filmRaitingSecond = prompt('Насколько оцените его', '');
+
+personalMovieDB.movies[lastWatchedFilmFirst] = filmRaitingFirst;
+personalMovieDB.movies[lastWatchedFilmSecond] = filmRaitingSecond;
+
+console.log(personalMovieDB.movies);
+
+
+
